71c025dd86be9cf1e5f64ce5f48836960a97e060
Nathan Vranicar <navzep@gmail.com>
2025-12-11 18:53:18 -0600

    created JS file

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function() {
+    var timestamp = new Date();
+
+    var event_json = {
+        'timestamp': timestamp,
+        'source': "Nathan's SA challenge site"
+    }
+
+    $('#event_btn').click(function() {
+        bt('track', 'button_clicked', event_json, {onComplete: function() {
+            alert('Event track completed!');
+        }});
+    });
+
+    var user_json = {
+        'user_id': 'nvranicar@zetaglobal.com',
+        'button_last_clicked_at': timestamp,
+        'has_clicked_button': 'Y'
+    }
+
+    $('#profile_btn').click(function() {
+        bt('updateUser', user_json, {onComplete: function() {
+            alert('Profile update completed!');
+        }});
+    });
+});
